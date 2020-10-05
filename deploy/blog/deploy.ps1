a45a61553a9d6ba5fec153ca03823e1bdaf13b62
Tom Meadon <tom.meadon@newsignature.com>
2020-10-05 14:26:52 +0100

    set the blob content type

--- deploy/blog/deploy.ps1
+++ deploy/blog/deploy.ps1
@@ -20,3 +20,24 @@ Enable-AzStorageStaticWebsite -Context $storageAccount.Context -IndexDocument 'i
 
 # deploy the files
 Get-ChildItem -Path '.\deploy\blog\site' -File -Recurse | Set-AzStorageBlobContent -Container '$web' -Context $storageAccount.Context -Force # -Properties @{ ContentType = "text/html; charset=utf-8"; }
+
+# set the content type on the blobs
+Get-AzStorageBlob Get-AzStorageBlob -Container '$web' -Context $storageAccount.Context | ForEach-Object -Parallel {
+
+    $extension = $_.Name.Split('.')[-1]
+
+    switch ($extension)
+    {
+        "html" { $contentType = 'text/html' }
+        "json" { $contentType = 'application/json' }
+        "xml" { $contentType = 'text/xml' }
+        "txt" { $contentType = 'text/plain' }
+        "css" { $contentType = 'text/css' }
+        "javascript" { $contentType = 'application/javascript' }
+        Default { $contentType = "" }
+    }
+
+    $_.ICloudBlob.Properties.ContentType = $contentType
+    $_.ICloudBlob.SetProperties()
+}
+
