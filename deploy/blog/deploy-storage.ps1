8022e6e16d8b2e31f0c83aecf02f4d0a9f36b407
Tom Meadon <tom.meadon@newsignature.com>
2020-10-10 08:54:57 +0100

    move to azcopy for file copy and remove storage deployment

--- deploy/blog/deploy-storage.ps1
+++ deploy/blog/deploy-storage.ps1
@@ -21,28 +21,5 @@ if (-not (Get-AzStorageAccount -StorageAccountName $storageAccountName -Resource
 $storageAccount = Get-AzStorageAccount -ResourceGroupName $rgName -AccountName $storageAccountName
 Enable-AzStorageStaticWebsite -Context $storageAccount.Context -IndexDocument 'index.html' -ErrorDocument404Path '404.html' -Verbose
 
-# deploy the files
-Get-ChildItem -Path '.\deploy\blog\site' -File -Recurse | Set-AzStorageBlobContent -Container '$web' -Context $storageAccount.Context -Force # -Properties @{ ContentType = "text/html; charset=utf-8"; }
-
-# set the content type on the blobs
-Get-AzStorageBlob -Container '$web' -Context $storageAccount.Context | ForEach-Object -Parallel {
-
-    $extension = $_.Name.Split('.')[-1]
-
-    switch ($extension)
-    {
-        "html" { $contentType = 'text/html' }
-        "json" { $contentType = 'application/json' }
-        "xml" { $contentType = 'text/xml' }
-        "txt" { $contentType = 'text/plain' }
-        "css" { $contentType = 'text/css' }
-        "javascript" { $contentType = 'application/javascript' }
-        Default { $contentType = "" }
-    }
-
-    $_.ICloudBlob.Properties.ContentType = $contentType
-    $_.ICloudBlob.SetProperties()
-}
-
 # enable azure storage logging
 Set-AzStorageServiceLoggingProperty -ServiceType Blob -LoggingOperations Read, Write, Delete -RetentionDays 365 -Version 2.0 -Context $storageAccount.Context
