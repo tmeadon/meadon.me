42c2c1b486f12af4e69faebed6cd96b00fd20d0e
Tom Meadon <tom@meadon.me>
2024-07-08 16:41:33 +0100

    Exclude draft blog posts from static paths and sort posts by publication date

--- src/content/config.ts
+++ src/content/config.ts
@@ -10,6 +10,7 @@ const blog = defineCollection({
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
 		summary: z.string().optional(),
+		draft: z.boolean().optional(),
 	}),
 });
 
