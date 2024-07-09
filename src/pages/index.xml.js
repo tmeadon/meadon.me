5989b426642eba6359de911f8b7b383e78990ce9
Tom Meadon <tom@meadon.me>
2024-07-09 09:29:49 +0100

    Update RSS description

--- src/pages/index.xml.js
+++ src/pages/index.xml.js
@@ -8,7 +8,7 @@ export async function GET(context) {
 	const posts = await getCollection('blog');
 	return rss({
 		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
+		description: "Recent content written by Tom Meadon",
 		site: context.site,
 		items: posts.map((post) => ({
 			...post.data,
